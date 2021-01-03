a2ab5100e0a5f63d26a7e00cd144b1f0af2032e6
CreativiTICs <creativiticscolombia@gmail.com>
2021-01-03 13:02:21 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,17 @@
-
-
 //Puerto
 process.env.PORT = process.env.PORT || 3000
+
+//Entorno
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//Base de datos
+let urlDB;
+
+// if(process.env.NODE_ENV === 'dev'){
+//     urlDB = 'mongodb://localhost:27017/cafe';
+// }else {
+    urlDB =
+      "mongodb+srv://cafe-node:1EcMr9TcbjP46o2B@cluster0.kowy3.mongodb.net/cafe?authSource=admin&replicaSet=atlas-cjkw5b-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true";
+// }
+
+process.env.URLDB = urlDB;
