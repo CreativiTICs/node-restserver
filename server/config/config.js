956762b60d9510b91b3b5a3f0f6316bdca0d7d2f
CreativiTICs <creativiticscolombia@gmail.com>
2021-01-03 13:25:23 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -10,8 +10,7 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/cafe';
 }else {
-    urlDB =
-      "mongodb+srv://cafe-node:1EcMr9TcbjP46o2B@cluster0.kowy3.mongodb.net/cafe?authSource=admin&replicaSet=atlas-cjkw5b-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true";
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
