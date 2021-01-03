bb00299c39f4423075ea33e38fbfb60de7c4f820
CreativiTICs <creativiticscolombia@gmail.com>
2021-01-03 13:12:02 -0500

    Config Update

--- server/config/config.js
+++ server/config/config.js
@@ -7,11 +7,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 //Base de datos
 let urlDB;
 
-// if(process.env.NODE_ENV === 'dev'){
-//     urlDB = 'mongodb://localhost:27017/cafe';
-// }else {
+if(process.env.NODE_ENV === 'dev'){
+    urlDB = 'mongodb://localhost:27017/cafe';
+}else {
     urlDB =
       "mongodb+srv://cafe-node:1EcMr9TcbjP46o2B@cluster0.kowy3.mongodb.net/cafe?authSource=admin&replicaSet=atlas-cjkw5b-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true";
-// }
+}
 
 process.env.URLDB = urlDB;
